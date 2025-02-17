858b7d40bbab7f3df3b44278af36c3542fca650f
Jin <Dowha132@yonsei.ac.kr>
2025-02-17 13:32:20 +0900

    ex4-7

---
+++ ch4/ex4-7.v
@@ -0,0 +1,18 @@
+module Top;
+//연결 변수 선언
+reg[3:0] A,B;
+reg C_IN;
+wire[3:0] SUM;
+wire C_OUT;
+
+    fulladd4 fa_ordered(SUM,C_OUT,A,B,C,C_IN);
+    //스티뮬러스
+endmodule
+
+module fulladd4(sum,c_out,a,b,c_in);
+output [3:0] sum;
+output c_out;
+input [3:0] a,b;
+input c_in;
+//모듈 내용
+endmodule
